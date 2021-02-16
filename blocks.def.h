5fdf70c9ce62f5978efc3f32cc6b5fb84eca16bf
Tung Beier <beiertu@mediamarktsaturn.com>
2021-02-16 20:43:36 +0100

    Swap datetime and volume order

--- blocks.def.h
+++ blocks.def.h
@@ -1,11 +1,12 @@
 static const Block blocks[] = {
 	/*	Icon	Command								Interval	Signal	*/
-	{	"🕓",		"datetime",						1,			0	},
+	{	"",			"echo dwm",						0,			10	},
 	{	"",			"volume",						0,			10	},
+	{	"🕓 ",		"datetime",						1,			0	},
 };
 
 // Sets delimeter between status commands.
 // NULL character ('\0') means no delimeter.
-static char delim[] = " ";
+static char delim[] = " | ";
 static unsigned int delimLen = 5;
 
