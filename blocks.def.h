c8851e4ef78fb00e5ef931670462683db01efacb
Tung Beier <beiertu@mediamarktsaturn.com>
2021-02-16 20:59:15 +0100

    Add delimeter

--- blocks.def.h
+++ blocks.def.h
@@ -1,11 +1,12 @@
 static const Block blocks[] = {
 	/*	Icon	Command								Interval	Signal	*/
+	{	"",			"echo dwm",						0,			10	},
 	{	"",			"volume",						0,			10	},
 	{	"🕓 ",		"datetime",						1,			0	},
 };
 
 // Sets delimeter between status commands.
 // NULL character ('\0') means no delimeter.
-static char delim[] = " ";
+static char delim[] = " | ";
 static unsigned int delimLen = 5;
 
