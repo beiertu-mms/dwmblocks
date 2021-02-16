8baec2a145e50fbe9835680c5f307db4c1b591c2
Tung Beier <beiertu@mediamarktsaturn.com>
2021-02-16 20:18:42 +0100

    Move datetime command to a script

--- blocks.def.h
+++ blocks.def.h
@@ -1,8 +1,6 @@
 static const Block blocks[] = {
 	/*	Icon	Command								Interval	Signal	*/
-	{	"",		"echo ''",							0,			0	},
-	{	"",		"date '+%H:%M:%S %a, %d %b %Y'",	1,			0	},
-	{	"",		"echo ' '",							0,			0	},
+	{	"🕓",		"datetime",						1,			0	},
 };
 
 //sets delimiter between status commands. NULL character ('\0') means no delimiter.
