037f18b52b2ea7084bf49d2904fe46b6d41ddbe8
Tung Beier <beiertu@mediamarktsaturn.com>
2021-02-23 19:57:54 +0100

    Use space for code alignment

--- blocks.def.h
+++ blocks.def.h
@@ -1,8 +1,8 @@
 static const Block blocks[] = {
-	/*	Icon	Command								Interval	Signal	*/
-	{	"",			"echo dwm",						0,			10	},
-	{	"",			"volume",						0,			10	},
-	{	"🕓 ",		"datetime",						1,			0	},
+	/* Icon     Command               Interval   Signal */
+	{  "",      "volume",             0,         10	},
+	{  "",      "datetime",           1,         0	},
+	{  "",      "echo dwm-6.2",       0,         0	},
 };
 
 //sets delimiter between status commands. NULL character ('\0') means no delimiter.
